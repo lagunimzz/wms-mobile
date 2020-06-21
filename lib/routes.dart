7a4d9b925667f369ed0151a2e89c24c7edca088b
samuch <samuch.work@gmail.com>
2020-06-21 23:00:55 +0700

    add product type screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/widgets.dart';
+import 'package:wmsmobile/product_type/product_type.dart';
 import 'package:wmsmobile/screens/example1/examplescreen1.dart';
 import 'package:wmsmobile/screens/example2/examplescreen2.dart';
 import 'package:wmsmobile/screens/DCManagement/ManagementScreen.dart';
@@ -9,4 +10,5 @@ final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
   "/ExScreen2": (BuildContext context) => ExScreen2(),
   "/Management": (BuildContext context) => ManagementScreen(),
   "/ZoneMain": (BuildContext context) => ZoneMain(),
+  "/ProductType": (BuildContext context) => ProductType(),
 };

e416e3abfa5b19918d18cc33842240a276f47b47
samuch.dev@gmail.com <samuch.dev@gmail.com>
2020-06-22 15:13:55 +0700

    add product type page

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/widgets.dart';
-import 'package:wmsmobile/product_type/product_type.dart';
 import 'package:wmsmobile/screens/example1/examplescreen1.dart';
 import 'package:wmsmobile/screens/example2/examplescreen2.dart';
 import 'package:wmsmobile/screens/DCManagement/ManagementScreen.dart';
+import 'package:wmsmobile/screens/product_type/product_type.dart';
 import 'package:wmsmobile/screens/zone_management/zone_main.dart';
 
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
